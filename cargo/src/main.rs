b12d0eb4adef1980bee2e3d384e4b4a96399bcd7
LS <the413reply@gmail.com>
2024-07-07 22:36:39 +0800

    Update main.rs
    to simple

--- cargo/src/main.rs
+++ cargo/src/main.rs
@@ -1,3 +1,22 @@
+// {% unless std -%}
+// #![no_std]
+// #![no_main]
+
+// {% endunless -%}
+// {% unless std -%}
+// #[no_mangle]
+// {% endunless -%}
+// fn main() {
+//     // It is necessary to call this function once. Otherwise some patches to the runtime
+//     // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
+//     esp_idf_svc::sys::link_patches();
+
+//     // Bind the log crate to the ESP Logging facilities
+//     esp_idf_svc::log::EspLogger::initialize_default();
+
+//     log::info!("Hello, world!");
+// }
+
 {% unless std -%}
 #![no_std]
 #![no_main]
@@ -6,13 +25,22 @@
 {% unless std -%}
 #[no_mangle]
 {% endunless -%}
+use esp_idf_sys as _;
+use log::info;
+use esp_idf_hal::{
+    peripherals::Peripherals,
+    gpio::PinDriver,
+    delay::FreeRtos};
+
 fn main() {
     // It is necessary to call this function once. Otherwise some patches to the runtime
     // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
     esp_idf_svc::sys::link_patches();
-
     // Bind the log crate to the ESP Logging facilities
     esp_idf_svc::log::EspLogger::initialize_default();
-
-    log::info!("Hello, world!");
+    info!("Main START");
+    // Initialize the peripherals
+    let peripherals = Peripherals::take().unwrap();
+    let mut led_pin = PinDriver::output(peripherals.pins.gpio48).unwrap(); //add the pin number here
+    
 }
